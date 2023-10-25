f339d8b7de64ca55efae3a740f3ec58e7f949159
Likeur off <102558960+Likeur@users.noreply.github.com>
2023-10-25 16:13:27 +0300

    freeportfolio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
